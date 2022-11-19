ef3fb314260774a74e804513b298b3a2baaa49e7
willianchu <willianchu@hotmail.com>
2022-11-18 17:51:06 -0800

    groups

---
+++ groups.js
@@ -0,0 +1,10 @@
+var Regex_Pattern = /__________/; //Do not delete '/'. Replace __________ with your regex.
+
+// Task
+
+// You have a test String S.
+// Your task is to write a regex which will match S with the following condition:
+
+// S should have 3 or more consecutive repetitions of ok.
+
+Regex_Pattern = /(ok){3,}/;
