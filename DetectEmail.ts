ceaf100bffe680c8111cd7abe54703ac8eba7875
willianchu <willianchu@hotmail.com>
2022-12-03 23:12:31 -0800

    in progress

---
+++ DetectEmail.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+function main() {
+    const numberLines: number = parseInt(readLine().trim(), 10);
+    const regex: RegExp = /([a-zA-Z0-9]+)@([a-zA-Z0-9]+)\.([a-zA-Z0-9]{1,3})/g;
+    let answer: string = '';
+    for (let i: number = 0; i < numberLines; i++) {
+      const line: string = readLine();
+      let email = regex.exec(line);
+      console.log(email );
+      if(regex.test(line) != null) {
+        answer += line.match(regex) + ';';
+      console.log(answer);
+      }
+      
+      
+  }
+
+  return answer;
+}
