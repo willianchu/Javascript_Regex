dc847c67553b0beede05a8951492484aabbd70cb
willianchu <willianchu@hotmail.com>
2022-12-04 21:08:17 -0800

    1 error

--- DetectEmail.ts
+++ DetectEmail.ts
@@ -21,22 +21,16 @@ function readLine(): string {
 
 function main() {
         const numberLines: number = parseInt(readLine().trim(), 10);
-    const regex: RegExp = /([a-zA-Z0-9]+)@([a-zA-Z0-9]+)\.([a-zA-Z0-9]{1,3})/g;
+    const regex: RegExp = /([a-zA-Z0-9_-]+)@([a-zA-Z0-9]+)\.([a-zA-Z0-9]{1,3})/g;
     let answer: string = '';
     for (let i: number = 0; i < numberLines; i++) {
       const line: string = readLine();
       if(regex.test(line)) {
-          console.log("quick");
           answer += line.match(regex) + ';';
-      console.log(answer);
       }
       
       
   }
-  //remone last ;
   answer = answer.slice(0, -1);
-  return answer.split(';').sort().join(';');
+console.log( answer.split(';').sort().join(';'));
 }
-
-bd@tnmbonline.com;customerservice@tnmbonline.com;ibd@tnmbonline.com;nricell@tnmbonline.com;tmbankhi@sancharnet.in
-bd@tnmbonline.com;customerservice@tnmbonline.com;ibd@tnmbonline.com;nricell@tnmbonline.com;ttn_tmbankhi@sancharnet.in
