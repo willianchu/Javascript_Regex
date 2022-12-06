99b6c80725419a80be22924367855b7a6cf10213
willianchu <willianchu@hotmail.com>
2022-12-05 22:16:58 -0800

    still 3 cases must solve spaces

--- DetectEmail.ts
+++ DetectEmail.ts
@@ -1,4 +1,7 @@
-'use strict';
+// not only word characters but "." can also be present in the part before @
+
+// emails ending in "." are not acceptable. for example "abc.fds@wohoo.co.in." is not acceptable
+// 'use strict';
 
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
@@ -21,15 +24,17 @@ function readLine(): string {
 
 function main() {
         const numberLines: number = parseInt(readLine().trim(), 10);
-    const regex: RegExp = /([a-zA-Z0-9_-]+)@([a-zA-Z0-9]+)\.([a-zA-Z0-9]{1,3})/g;
+    const regex: RegExp = /[.\w]+@\w+(?:.\w+)+/g;
     // passed in 2 cases 0 and 1
     // const regex: RegExp = /([a-zA-Z0-9_-]+)@([a-zA-Z0-9]+)\.([a-zA-Z0-9]{1,3})/g;
     // passed in 3 cases in cases 0 1 3
-    // const regex: RegExp = /\b(?:\w+\.)*\w+@(?:\w+\.)+\w+/;
+    // const regex: RegExp = /(\S+@\S+\.\w+)/g;
+    // const regex: RegExp = \b[\w\.]+@\w+\.[\w\.]+\b;
     // const regex: RegExp = /[\w\.]+@\w+\.[\.\w]*\w/g;
     // const regex: RegExp =[\w_\.]+@[\w_\.]+\.[a-z]{2,3};
-    // const regex: RegExp = \b[\w\.]+@\w+\.[\w\.]+\b;
+    // const regex: RegExp = /\b(?:\w+\.)*\w+@(?:\w+\.)+\w+/;
     // const regex: RegExp = /\b([.a-zA-Z0-9_\/]*@[a-zA-Z0-9_\/]*\.[a-zA-Z0-9_\/.]*)\b/g;
+    // const regex: RegExp = /\b[\da-zA-Z\._\-]+@[a-zA-Z\.]+\.[a-zA-Z]{1,3}\b/;
     let answer: string = '';
     for (let i: number = 0; i < numberLines; i++) {
       const line: string = readLine();
