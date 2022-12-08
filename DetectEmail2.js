ee1e988517217c03194ac26a00496bdb85cd01e9
willianchu <willianchu@hotmail.com>
2022-12-07 22:09:47 -0800

    version 2 and 3

--- DetectEmail2.js
+++ DetectEmail2.js
@@ -8,6 +8,17 @@ function processData(input) {
 //   console.log([...new Set(input.match(exp)?.sort())].join(';'));
 // } 
 
+// function processData(input) {
+//   //Enter your code here
+//   var unique = {};
+  
+//   input.match(/\b[A-Za-z0-9_.]+@[a-z]+(\.[a-z]+)+\b/g).sort().map(function (email) {
+//       unique[email] = null;
+//   });
+  
+//   console.log(Object.keys(unique).join(';'));
+// } 
+
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
 _input = "";
