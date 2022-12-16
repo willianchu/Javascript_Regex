b66d2fb2cf7707ef3c93287a1152c26114104269
willianchu <willianchu@hotmail.com>
2022-12-15 21:28:50 -0800

    passed just scannning hackerhank

--- hackerhankTweets.ts
+++ hackerhankTweets.ts
@@ -20,5 +20,15 @@ function readLine(): string {
 }
 
 function main() {
-    // Enter your code here
+    const n: number = parseInt(readLine().trim(), 10);
+    // look for hackerrank in the tweet upper and lower
+    const regex: RegExp = /hackerrank/i;
+    let numberOfHackerrankTweets: number = 0;
+    for (let i: number = 0; i < n; i++) {
+        const tweet: string = readLine().trim();
+        if (regex.test(tweet)) {
+            numberOfHackerrankTweets++;
+        }
+    }
+    console.log(numberOfHackerrankTweets);
 }
