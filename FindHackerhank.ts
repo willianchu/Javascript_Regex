22b98c75eb4c036fde016dccb5cb0d8df9cbb6c9
willianchu <willianchu@hotmail.com>
2022-12-18 21:30:08 -0800

    nested if's

--- FindHackerhank.ts
+++ FindHackerhank.ts
@@ -21,26 +21,21 @@ function readLine(): string {
 
 // For every line,
 
+// a- Print 0 if the conversation starts and ends with hackerrank
 // a- Print 1 if the conversation starts with hackerrank
 // b- Print 2 if the conversation ends with hackerrank
-// c- Print 0 if the conversation starts and ends with hackerrank
 // d- Print -1 if none of the above.
 function main() {
     // Enter your code here
     const n: number = parseInt(readLine().trim(), 10);
-    const regex: RegExp = /hackerrank/gi;
     for (let i: number = 0; i < n; i++) {
         const line: string = readLine();
-        if (line.match(regex)) {
-            if (line.match(/^hackerrank/gi)) {
-                if (line.match(/hackerrank$/gi)) {
-                    console.log(0);
-                } else {
-                    console.log(1);
-                }
-            } else {
-                console.log(2);
-            }
+        if(line.match(/^hackerrank/gi) && line.match(/hackerrank$/gi)) {
+            console.log(0);
+        }	else if(line.match(/^hackerrank/gi)) {
+            console.log(1);
+        }  else if(line.match(/hackerrank$/gi)) {
+            console.log(2);
         } else {
             console.log(-1);
         }
