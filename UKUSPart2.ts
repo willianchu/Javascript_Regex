64396570ae88daf0da4590f699d837d7aa660a8b
willianchu <willianchu@hotmail.com>
2022-12-22 20:25:19 -0800

    copy of part 1 substitution

--- UKUSPart2.ts
+++ UKUSPart2.ts
@@ -23,18 +23,17 @@ function main() {
     const n: number = parseInt(readLine().trim(), 10);
     const words: string[] = [];
     for (let i: number = 0; i < n; i++) {
-        words.push(readLine().trim());
+      words.push(readLine().trim());
     }
     const t: number = parseInt(readLine().trim(), 10);
     for (let i: number = 0; i < t; i++) {
-        let UKWord: string = readLine().trim();
-        let USWord: string = UKWord.replace(/our/g, 'or');
-        let count: number = 0;
-        for (let j: number = 0; j < n; j++) {
-            if (words[j] === UKWord || words[j] === USWord) {
-                count++;
-            }
-        }
-        console.log(count);
+      let UKWord: string = readLine().trim();
+      let USWord: string = UKWord.replace(/ou$/, 'o');
+      let count: number = 0;
+      for (let j: number = 0; j < n; j++) {
+        count += words[j].split(' ').filter((word: string) => word === UKWord || word === USWord).length;
+      }
+      console.log(count);
     }
-}
+    
+  }
