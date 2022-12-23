1c3f764fd2889f81c57dc5915b2845da00b9a135
willianchu <willianchu@hotmail.com>
2022-12-23 15:27:57 -0800

    save enunciate

--- UtopianIdentificationNumber.ts
+++ UtopianIdentificationNumber.ts
@@ -1,3 +1,9 @@
+// A new identification number is given for every Citizen of the Country Utopia and it has the following format.
+
+// The string must begin with between 0-3 (inclusive) lowercase letters.
+// Immediately following the letters, there must be a sequence of digits (0-9). The length of this segment must be between 2 and 8, both inclusive.
+// Immediately following the numbers, there must be atleast 3 uppercase letters.
+// Your task is to find out if a given identification number is valid or not.
 'use strict';
 
 process.stdin.resume();
