9d0d8321098ff75a52dc19e147af112b71da4821
willianchu <willianchu@hotmail.com>
2023-01-17 22:06:16 -0800

    intersection using filter

--- BasicsOfSetsAndRelations.js
+++ BasicsOfSetsAndRelations.js
@@ -1,12 +1,19 @@
-function unionOfSets(setA, setB) {
-  let union = new Set(setA);
-  for (let elem of setB) {
-    union.add(elem);
-  }
-  return union.size;
-}
+// function unionOfSets(setA, setB) {
+//   let union = new Set(setA);
+//   for (let elem of setB) {
+//     union.add(elem);
+//   }
+//   return union.size;
+// }
 
-const A = new Set([1, 2, 3, 4, 5, 6, 6]);
+const A = new Set([1, 2, 3, 4, 5, 6]);
 const B = new Set([2, 3, 4, 5, 6, 7, 8]);
 
-console.log(unionOfSets(A, B)); // answer is 8
+// refactor
+// Union A ∪ B
+const union = new Set([...A, ...B]);
+console.log(union.size); // Set { 1, 2, 3, 4, 5, 6, 7, 8 }
+
+// Intersection A ∩ B
+const intersection = new Set([...A].filter(x => B.has(x)));
+console.log(intersection.size); // Set { 2, 3, 4, 5, 6 }
