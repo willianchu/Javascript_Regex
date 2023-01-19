e6ba3e885dae242ff084fd9b90dd1f9683374a70
willianchu <willianchu@hotmail.com>
2023-01-18 21:36:56 -0800

    insert SQL version

--- BasicsOfSetsAndRelations.js
+++ BasicsOfSetsAndRelations.js
@@ -14,13 +14,23 @@ const B = new Set([2, 3, 4, 5, 6, 7, 8]);
 const union = new Set([...A, ...B]);
 console.log(union.size); // Set { 1, 2, 3, 4, 5, 6, 7, 8 }
 
+// in sql
+// SELECT * FROM A UNION SELECT * FROM B;
+
+
 // Intersection A ∩ B
 const intersection = new Set([...A].filter(x => B.has(x)));
 console.log(intersection.size); // Set { 2, 3, 4, 5, 6 }
+// in sql
+// SELECT * FROM A INTERSECT SELECT * FROM B;
+
 
 // Difference A - B
 const difference = new Set([...A].filter(x => !B.has(x)));
 console.log(difference.size); // Set { 1 }
+// in sql
+// SELECT * FROM A EXCEPT SELECT * FROM B;
+
 
 // Cartesian Product A × B
 let add2new = new Set();
@@ -30,6 +40,9 @@ for(let a of A) {
   }
 }
 console.log(add2new.size); 
+// in sql
+// SELECT * FROM A CROSS JOIN B;
+
 
 // Refactor Cartesian Product A × B
 const cartesianProduct = new Set([...A].map(a => [...B].map(b => [a, b])).flat());
